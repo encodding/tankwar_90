ac2afadd3951eb792416391b099081bc1b7d1a60
cvEngineer <1211781617@qq.com>
2024-05-15 15:51:40 +0800

    delete

--- src/main/java/cn/play/freely/game/tank/main/Main.java
+++ src/main/java/cn/play/freely/game/tank/main/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         TankGame.get();
-//        convertImage(new File("E:\\Workspace\\Java\\Playground\\Tank\\src\\main\\resources\\textures\\ui\\UIView.png"));
     }
 }
